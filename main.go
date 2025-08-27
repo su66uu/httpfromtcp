f7e0fd03e128440d403c99c4a6bc69f1e17d8113
Subbu <svenkat683@gmail.com>
2025-08-27 20:42:08 +0530

    refactor: run the reading the lines part in a go routine
    Yay! this is my first go routine

--- main.go
+++ main.go
@@ -3,15 +3,47 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-func fClose(f *os.File) {
-	err := f.Close()
-	if err != nil {
-		log.Fatal("Error closing the file", "error", err)
-	}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		defer func() {
+			if err := f.Close(); err != nil {
+				log.Printf("Error closing the file %v", err)
+			}
+		}()
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			fmt.Printf("read: %s\n", str)
+		}
+	}()
+
+	return out
 }
 
 func main() {
@@ -20,28 +52,8 @@ func main() {
 		log.Fatal("Error", "error", err)
 	}
 
-	defer fClose(f)
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
